2994ee0c1033c86586c704865d6a19c0c57501bc
iSOcH <github@pascal-schwarz.ch>
2025-01-26 09:52:54 +0000

    module 3 structured data

--- examples/2-creating-structs/creating/src/main.rs
+++ examples/2-creating-structs/creating/src/main.rs
@@ -3,6 +3,14 @@ struct Person {
     first_name: String,
     last_name: String,
     age: Option<u8>,
+    email: Option<String>,
+    phone_number: Option<String>,
+}
+
+impl Person {
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
 }
 
 fn main() {
@@ -10,7 +18,25 @@ fn main() {
         first_name: "Alfredo".to_string(),
         last_name: "Sanchez".to_string(),
         age: Some(23),
+        email: "some-address@example.org".to_string().into(), // .into() alone does not error in RustRover, but it does not work since this needs both to_string and wrapping in Option
+        phone_number: None
     };
+
     println!("The person's first name is: {}", alfredo.first_name);
     println!("The person's age is: {:?}", alfredo.age);
+
+    println!("Person as a whole: {alfredo:?}");
+
+    // It seems to not be supported to call functions in the format string, "Full name: {alfredo.full_name()}" does not compile
+    println!("Full name: {full_name}", full_name=alfredo.full_name());
 }
+
+/*
+What is a struct in Rust, and what purpose does it serve? How is it different from other data types in the language?
+    It assembles multiple values of potentially different types. Compared to tuple, the properties are named.
+
+In the provided code, why is the `#[derive(Debug)]` attribute used for the `Person` struct? What benefit does it provide?
+    Generates an implementation for the Debug trait which allows formatting using {:?}
+
+Challenge questions see above
+*/
--- examples/3-associated-functions/associated/src/main.rs
+++ examples/3-associated-functions/associated/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
@@ -14,9 +15,18 @@ impl User {
             active: true,
         }
     }
+
+    fn from_email(email: String) -> Option<Self> {
+        let user_and_uri = email.split_once('@');
+        let option_of_user = user_and_uri.map(|(username, uri)| User::new(username.to_string(), email.to_string(), uri.to_string()));
+        option_of_user
+    }
+
     fn deactivate(&mut self) {
         self.active = false;
     }
+
+    fn update_uri(&mut self, uri: String) { self.uri = uri; }
 }
 
 fn main() {
@@ -29,4 +39,21 @@ fn main() {
     println!("Account {} status is: {}", new_user.username, new_user.active);
     new_user.deactivate();
     println!("Account {} status is: {}", new_user.username, new_user.active);
+
+    let user_from_email = User::from_email(new_user.email);
+    match user_from_email {
+        Some(mut user) => {
+            user.update_uri("https://alfredodeza.com".to_string());
+            println!("From email: {user:?}");
+        },
+        None => println!("user_from_email returned None"),
+    }
 }
+
+/*
+What is an associated function in Rust, and how does it differ from regular methods? In what scenarios would you choose to use an associated function instead of a regular method?
+    Very similar to "static" functions in languages like C#. A function which can be called without having an instance of the type. Often used to create instances (factory function).
+
+In the provided code, why is the associated function `new` used to create a new instance of the `User` struct? What advantages does it offer compared to creating a struct instance directly?
+    It defaults `active` to true, removing the need to specify it to create a `User`.
+ */
